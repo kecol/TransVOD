40e7f968e5f56560752f9de59c91952c8333bf1d
kecol <ezequiel.bidart@gmail.com>
2022-04-17 08:05:11 -0700

    some configs updated

--- configs/r50_eval_single_toy_baseline.sh
+++ configs/r50_eval_single_toy_baseline.sh
@@ -3,19 +3,20 @@
 set -x
 T=`date +%m%d%H%M`
 
-EXP_DIR=exps/singlebaseline/toy_r50_e10_nf4_ld6,7_lr0.0002_nq25_bs8_wbox_joint_MEGA_detrNorm_class4_pretrain_coco_dc5
-mkdir ${EXP_DIR}
+EXP_DIR=exps/baselines/toy_r50_e100
+mkdir -p ${EXP_DIR}
 PY_ARGS=${@:1}
 python -u main.py \
     --num_classes 4 \
     --vid_path ${TOY_DIR} \
-    --epochs 10 \
-    --num_feature_levels 1\
+    --epochs 1 \
+    --eval \
+    --num_feature_levels 1 \
     --num_queries 25 \
     --dilation \
-    --batch_size 8 \
-    --num_workers 8 \
-    --lr_drop_epochs 6 7 \
+    --batch_size 1 \
+    --resume ${EXP_DIR}/checkpoint0041.pth \
+    --num_workers 12 \
     --with_box_refine \
     --dataset_file toy_single \
     --output_dir ${EXP_DIR} \
--- configs/r50_train_single_toy_baseline.sh
+++ configs/r50_train_single_toy_baseline.sh
@@ -3,20 +3,23 @@
 set -x
 T=`date +%m%d%H%M`
 
-EXP_DIR=exps/singlebaseline/toy_r50_e10_nf4_ld6,7_lr0.0002_nq25_bs8_wbox_joint_MEGA_detrNorm_class4_pretrain_coco_dc5
+EXP_DIR=exps/baselines/toy_r50_e100
 mkdir ${EXP_DIR}
 PY_ARGS=${@:1}
 python -u main.py \
     --num_classes 4 \
     --vid_path ${TOY_DIR} \
-    --epochs 10 \
-    --num_feature_levels 1\
+    --epochs 100 \
+    --num_feature_levels 1 \
     --num_queries 25 \
     --dilation \
-    --batch_size 8 \
+    --batch_size 16 \
     --num_workers 8 \
-    --lr_drop_epochs 6 7 \
+    --lr 1e-4 \
+    --lr_drop_epochs 40 60 80 \
     --with_box_refine \
     --dataset_file toy_single \
+    --coco_pretrain \
+    --resume ./exps/singlebaseline/toy_r50_e10_nf4_ld6,7_lr0.0002_nq25_bs8_wbox_joint_MEGA_detrNorm_class4_pretrain_coco_dc5/checkpoint0009.pth \
     --output_dir ${EXP_DIR} \
     ${PY_ARGS} 2>&1 | tee ${EXP_DIR}/log.train.$T
